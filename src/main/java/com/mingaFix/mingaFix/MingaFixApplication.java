a95b644146465ce05d031055e3802981a9c27de6
Einarr07 <aidenperse@gmail.com>
2025-10-01 15:32:10 -0500

    Project initialization

---
+++ src/main/java/com/mingaFix/mingaFix/MingaFixApplication.java
@@ -0,0 +1,13 @@
+package com.mingaFix.mingaFix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingaFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingaFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mingaFix/mingaFix/MingaFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingaFix.mingaFix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingaFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
